6fe45e92a9ab948aad6583b317b955a66e0317ea
Mohak327 <mohak327@gmail.com>
2020-12-01 19:45:16 +0530

    vsSettings

--- change.js
+++ change.js
@@ -3,20 +3,19 @@ var tool = canvas.getContext("2d");
 canvas.height = "600";
 canvas.width = "1200";
 
-
-function draw(event){
+function draw(event) {
 	var x = event.clientX;
 	var y = event.clientY;
 	tool.strokeStyle = "red";
-	tool.lineTo(x,y)
+	tool.lineTo(x, y)
 	tool.stroke();
 }
 
-function start(event){
-	window.addEventListener("mousemove",draw);
+function start(event) {
+	window.addEventListener("mousemove", draw);
 }
 
-function clearCanvas(){
-	removeEventListener("mousemove",draw);
-	tool.clearRect(0,0,window.innerWidth,window.innerHeight);
+function clearCanvas() {
+	removeEventListener("mousemove", draw);
+	tool.clearRect(0, 0, window.innerWidth, window.innerHeight);
 }
